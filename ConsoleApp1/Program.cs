520a989ebddf4f35e8f3cb558483d016b10edbe4
PredatoR2325 <35430161+PredatoR2325@users.noreply.github.com>
2019-11-01 17:46:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static double Division(double x, double y)
+        {
+            return x / y;
+        }
+
+        public static double Addition(double x, double y)
+        {
+            return x + y;
+        }
+
+    }
+}
